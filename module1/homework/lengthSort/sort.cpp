4851e074b688c9792bd203e233914925abf49bd0
piezol <piezolpiezol@gmail.com>
2020-06-07 15:31:48 +0200

    Sorting of strings works

---
+++ module1/homework/lengthSort/sort.cpp
@@ -0,0 +1,37 @@
+#include "sort.hpp"
+
+#include <iostream>
+auto iterToInsertIntoSortedDeque(const std::deque<std::string>& sortedDeque,
+                                 const std::string& strToInsert) {
+    if (sortedDeque.size() == 0)
+        return sortedDeque.end();
+
+    size_t leftBound = 0;
+    auto rightBound = sortedDeque.size();
+
+    do {
+        auto middle = leftBound + (rightBound - leftBound) / 2;
+        const auto& middleElement = sortedDeque[middle];
+        if (middleElement.size() > strToInsert.size() or
+            (middleElement.size() == strToInsert.size() and middleElement >= strToInsert)) {  // should be a predicate but we don't know them yet :)
+            rightBound = middle;
+        } else {
+            leftBound = std::max(leftBound + 1, middle);
+        }
+
+    } while (leftBound < rightBound);
+
+    if (leftBound == sortedDeque.size())
+        return sortedDeque.end();
+
+    return std::next(sortedDeque.begin(), leftBound);
+}
+
+std::deque<std::string> lengthSort(std::forward_list<std::string>& fList) {
+    std::deque<std::string> sortedStrings;
+    for (const auto& str : fList) {
+        auto posToInsert = iterToInsertIntoSortedDeque(sortedStrings, str);
+        sortedStrings.insert(posToInsert, str);
+    }
+    return sortedStrings;
+}
